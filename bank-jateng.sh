ea4dd62c096066eae7d3cf73909993015fcf0f8e
Enkzh@CPAlytics <shuenk789@gmail.com>
2024-10-20 02:51:15 +0700

    Update bank-jateng.sh

--- bank-jateng.sh
+++ bank-jateng.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BANKCODE="jawa_tengah"
+
 export TZ="Asia/Jakarta"
 response=$(curl -s 'https://www.bankjateng.co.id/api/public/kurs/?page_size=99' \
   -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
