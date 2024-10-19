9926b0cb2b24672e8b2c0f75a15728fc03d0e434
Enkzh@CPAlytics <shuenk789@gmail.com>
2024-10-19 21:30:43 +0700

    Update bjb.sh
    FETCH KURS BANK BJB

--- bjb.sh
+++ bjb.sh
@@ -1,3 +1,5 @@
+# Set timezone ke WIB (UTC+7)
+export TZ='Asia/Jakarta'
 response=$(curl -s 'https://bankbjb.co.id/currency/filter' \
   -X 'POST' \
   -H 'Accept: */*' \
@@ -21,12 +23,14 @@ response=$(curl -s 'https://bankbjb.co.id/currency/filter' \
 ttBeli=$(echo $response | jq -r '.data[] | select(.code == "USD") | .counterBuy')
 ttJual=$(echo $response | jq -r '.data[] | select(.code == "USD") | .counterSell')
 
-# Format angka sesuai permintaan, tambahkan koma untuk ribuan dan desimal
-ttBeli=$(printf "%'.2f\n" $ttBeli | sed 's/\./,/g')
-ttJual=$(printf "%'.2f\n" $ttJual | sed 's/\./,/g')
+# Format angka, tambahkan koma untuk ribuan dan desimal
+ttBeli=$(printf "%'.2f\n" $ttBeli | sed 's/\./,/g' | sed ':a;s/\B[0-9]\{3\}\(\,\|$\)/.&/;ta')
+ttJual=$(printf "%'.2f\n" $ttJual | sed 's/\./,/g' | sed ':a;s/\B[0-9]\{3\}\(\,\|$\)/.&/;ta')
+lastUpdate=$(date '+%d/%m/%y - %H.%M WIB')
 
 # Kirim data ke Cloudflare KV Storage dengan key "bjb"
 curl -X PUT "https://api.cloudflare.com/client/v4/accounts/f60335e0aa3a7f534a9ed799d5192a34/storage/kv/namespaces/b2282cc52f25464882a822bd11ceb664/values/bjb" \
   -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
   -H "Content-Type: application/json" \
-  --data-raw "{\"bank\":\"bjb\",\"ttBeli\":\"${ttBeli}\",\"ttJual\":\"${ttJual}\"}"
+  # --data-raw "{\"bank\":\"bjb\",\"ttBeli\":\"${ttBeli}\",\"ttJual\":\"${ttJual}\"}"
+  --data-raw "{\"bank\":\"bjb\",\"ttBeli\":\"${ttBeli}\",\"ttJual\":\"${ttJual}\",\"lastUpdate\":\"${lastUpdate}\"}"
